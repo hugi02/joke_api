6ee04fce4989068277b85b19b16fe74ad8489d40
hugi02 <128356109+hugi02@users.noreply.github.com>
2025-11-20 15:04:55 +0200

    Update index.js
    index päivitys

--- index.js
+++ index.js
@@ -1,87 +1,32 @@
 const express = require('express');
-const LimitingMiddleware = require('limiting-middleware');
-const { types, randomJoke, randomTen, randomSelect, jokeByType, jokeById, count } = require('./handler');
-
 const app = express();
 
-app.use(new LimitingMiddleware().limitByIp());
+// Lataa vitsit JSON-tiedostosta
+const jokes = require('./index.json');
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
-
+// Root endpoint
 app.get('/', (req, res) => {
-  res.send('Tervetuloa omaan päivitettyyn Jokes API -sovellukseeni!');
+  res.send('Tervetuloa päivitettyyn Jokes API:in! Käytä /jokes, /jokes/random tai /jokes/count');
 });
 
+// Kaikki vitsit
+app.get('/jokes', (req, res) => {
+  res.json(jokes);
+});
+
+// Satunnainen vitsi
+app.get('/jokes/random', (req, res) => {
+  const randomIndex = Math.floor(Math.random() * jokes.length);
+  res.json(jokes[randomIndex]);
+});
+
+// UUSI: vitsien määrä
 app.get('/jokes/count', (req, res) => {
   res.json({ count: jokes.length });
 });
 
-
-app.get('/ping', (req, res) => {
-  res.send('pong');
+// Render / local server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
-
-
-app.get('/jokes/random', (req, res) => {
-  res.json(randomJoke());
-});
-
-app.get("/jokes/random/:num", (req, res) => {
-  let num;
-  try {
-    num = parseInt(req.params.num);
-    if (!num) {
-      res.send("The passed path is not a number.");
-    } else {
-      if (num > count) {
-        res.send(`The passed path exceeds the number of jokes (${count}).`);
-      } else {
-        res.json(randomSelect(num));
-      }
-    }
-  } catch (e) {
-    return next(e);
-  } 
-});
-
-app.get('/jokes/ten', (req, res) => {
-  res.json(randomTen());
-});
-
-app.get('/jokes/:type/random', (req, res) => {
-  res.json(jokeByType(req.params.type, 1));
-});
-
-app.get('/jokes/:type/ten', (req, res) => {
-  res.json(jokeByType(req.params.type, 10));
-});
-
-app.get('/jokes/:id', (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const joke = jokeById(+id);
-    if (!joke) return next({ statusCode: 404, message: 'joke not found' });
-    return res.json(joke);
-  } catch (e) {
-    return next(e);
-  }
-});
-
-app.get('/types', (req, res, next) => {
-  res.json(types);
-})
-
-app.use((err, req, res, next) => {
-  const statusCode = err.statusCode || 500;
-
-  res.status(statusCode).json({
-    type: 'error', message: err.message
-  });
-});
-
-const PORT = process.env.PORT || 3005;
-app.listen(PORT, () => console.log(`listening on ${PORT}`));
-
